700f09e0ac96e0577864dce89108dd878bd41d6b
Valerii Zimichev <vzimichev@MacBook-Air-Valerii.local>
2025-12-03 19:43:37 +0400

    imports

--- worker/celery_app.py
+++ worker/celery_app.py
@@ -15,3 +15,5 @@ celery_app.conf.result_serializer = "json"
 celery_app.conf.accept_content = ["json"]
 celery_app.conf.broker_heartbeat = 30
 celery_app.conf.broker_connection_retry_on_startup = True
+
+import worker.tasks
