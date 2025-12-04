199110f0ed6ab054256f8315c60e5872271cb962
vzimichev <55046330+vzimichev@users.noreply.github.com>
2025-12-04 18:56:03 +0400

    Redis (#9)

--- shared/config.py
+++ shared/config.py
@@ -8,11 +8,10 @@ class Settings(BaseSettings):
     telegram_token: str
     webhook_secret_token: Optional[str] = None
 
-    # RabbitMQ
-    rabbitmq_host: str
-    rabbitmq_port: int
-    rabbitmq_user: str
-    rabbitmq_password: str
+    # Redis
+    redis_host: str
+    redis_port: int
+    redis_password: str
 
     class Config:
         env_file = ".env"
